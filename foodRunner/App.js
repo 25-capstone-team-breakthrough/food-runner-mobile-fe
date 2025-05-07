8df282521d363028aa91131c7139b2f3502d2832
김도엽 <kimdoyeop@gimdoyeob-ui-MacBookAir.local>
2025-05-07 15:20:26 +0900

    App.js 수정

--- foodRunner/App.js
+++ foodRunner/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import AppNavigator from './navigation/AppNavigator';
 // App.js
 import { ExerciseProvider } from './context/ExerciseContext';
